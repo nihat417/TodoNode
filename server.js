f2bc3c23d8e964e2c147774f304b3c01f3953e65
nihat417 <akreminihat@gmail.com>
2024-05-09 22:44:36 +0400

    Added cluster

--- server.js
+++ server.js
@@ -5,9 +5,32 @@ const mongoose = require('mongoose');
 const todoRoutes = require('./routes/todoRoutes');
 const authRoutes = require('./routes/authRoutes');
 const DATABASE_URL = process.env.DATABASE_URL;
+const cors = require('cors');
+const cluster = require('cluster');
+const os = require('os');
 
 const app =express();
+const cpNum = os.cpus().length;
+
+
 app.use(express.json());
+app.use(cors());
+
+
+if(cluster.isMaster){
+    for (let index = 0; index < cpNum; index++) {
+        cluster.fork();
+    }
+    cluster.on('exit',(worker,code,signal)=>{
+        console.log(`worker with pid ${worker.process.pid} died`);
+        cluster.fork();
+    });
+}else{
+    app.listen(4000,()=>{
+        console.log(`server running on ${process.pid} @ 4000`);
+    });
+}
+
 
 
 mongoose
@@ -19,4 +42,4 @@ app.use('/todos',todoRoutes);
 app.use('/auth',authRoutes);
 
 
-app.listen(4000,()=>{console.log("server running on 4000 port")})
+// app.listen(4000,()=>{console.log("server running on 4000 port")})
